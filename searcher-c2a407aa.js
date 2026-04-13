ed5c0f9347166129cea2bd06baf7246110b03b73
karim <karim.dev@gmail.com>
2026-04-13 15:07:15 +0200

    Update book from 9407131

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-5adc9aff.js',
+                path_to_root + 'searchindex-d6e97e0c.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');

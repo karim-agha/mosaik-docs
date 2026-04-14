c48e6661b80a1ce092c1975856da19981aacce56
karim <karim.dev@gmail.com>
2026-04-14 02:43:16 +0200

    Update book from 403b99b

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-6e59478a.js',
+                path_to_root + 'searchindex-9e72bff4.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');

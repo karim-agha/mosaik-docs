3d90d4a85113ef65adc098425b950c53d36ecaa0
karim <karim.dev@gmail.com>
2026-08-28 07:50:09 +0200

    Update book from 872858c

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-49c8e102.js',
+                path_to_root + 'searchindex-e179421a.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
